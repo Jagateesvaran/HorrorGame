487d7137a912c2939aeb6b97d3eb197a98b77d1b
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-04-16 01:29:41 +0800

    Add light and rotch animation

---
+++ HorrorGame/Assets/Scriptes/FlameAnimations.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameAnimations : MonoBehaviour
+{
+
+    public int LightMode;
+    public GameObject FlameLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LightMode == 0)
+        {
+            StartCoroutine(AnimateLight());
+        }
+    }
+
+    IEnumerator AnimateLight()
+    {
+        LightMode = Random.Range(1, 4);
+        if (LightMode == 1)
+        {
+            FlameLight.GetComponent<Animation>().Play("TorchAnim1");
+        }
+        else if (LightMode == 2)
+        {
+            FlameLight.GetComponent<Animation>().Play("TorchAnim2");
+        }
+        else if (LightMode == 3)
+        {
+            FlameLight.GetComponent<Animation>().Play("TorchAnim3");
+        }
+
+        yield return new WaitForSeconds(0.99f);
+        LightMode = 0;
+    }
+
+}
