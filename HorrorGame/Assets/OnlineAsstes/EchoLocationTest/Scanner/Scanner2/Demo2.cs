a41803d61cc3df841b339fa5835d2c540ee6103e
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-05-09 02:21:14 +0800

    Do the mark

--- HorrorGame/Assets/OnlineAsstes/EchoLocationTest/Scanner/Scanner2/Demo2.cs
+++ HorrorGame/Assets/OnlineAsstes/EchoLocationTest/Scanner/Scanner2/Demo2.cs
@@ -155,12 +155,12 @@ public class Demo2 : MonoBehaviour
 	void Update()
 	{
 
-		//m_Range = mic.volumeinput + 10;
+		m_Range = mic.volumeinput + 10;
 
-		//if (mic.volumeinput > 10)
-		//{
-		//	StartCoroutine(ExampleCoroutine());
-		//}
+		if (mic.volumeinput > 10)
+		{
+			StartCoroutine(ExampleCoroutine());
+		}
 
 		for (int i = 0; i < m_Fxs.Length; i++)
 		{
