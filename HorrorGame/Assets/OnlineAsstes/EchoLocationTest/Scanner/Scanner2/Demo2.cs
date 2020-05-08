0797e51275598fbf0a5a48b801301f073d8f0b73
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-05-09 02:59:16 +0800

    Do more on Monday

--- HorrorGame/Assets/OnlineAsstes/EchoLocationTest/Scanner/Scanner2/Demo2.cs
+++ HorrorGame/Assets/OnlineAsstes/EchoLocationTest/Scanner/Scanner2/Demo2.cs
@@ -147,7 +147,7 @@ public class Demo2 : MonoBehaviour
 
 		m_FxType = Scanner.ScannerObject.FxType.FT_TransparencyTextured;
 
-		m_Range = 10;
+		m_Range = 15;
 
 
 
@@ -155,12 +155,12 @@ public class Demo2 : MonoBehaviour
 	void Update()
 	{
 
-		m_Range = mic.volumeinput + 10;
+		//m_Range = mic.volumeinput + 10;
 
-		if (mic.volumeinput > 10)
-		{
-			StartCoroutine(ExampleCoroutine());
-		}
+		//if (mic.volumeinput > 10)
+		//{
+		//	StartCoroutine(ExampleCoroutine());
+		//}
 
 		for (int i = 0; i < m_Fxs.Length; i++)
 		{
