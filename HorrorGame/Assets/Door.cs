203c6c630391305b2be0a5984ae21307c9fe14cf
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-05-14 21:40:06 +0800

    doing door

---
+++ HorrorGame/Assets/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Door : MonoBehaviour
+{
+
+    public GameObject UIInteractionCanvas;
+    public Animation openDoorAnimation;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            UIInteractionCanvas.gameObject.SetActive(true);
+            Debug.Log("in Room Range");
+
+
+            if (Input.GetKeyDown("e"))
+            {
+                openDoorAnimation.Play();
+            }
+        }
+    }
+
+
+
+
+}
