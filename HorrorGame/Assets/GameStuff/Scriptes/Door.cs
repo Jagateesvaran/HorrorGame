06abdcf596775cbd9a420956fbb7239886b7ca55
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-06-07 02:26:10 +0800

    Tmr do alot of work

---
+++ HorrorGame/Assets/GameStuff/Scriptes/Door.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Door : MonoBehaviour
+{
+
+    public GameObject UIInteractionCanvas;
+    public Animation openDoorAnimation;
+    public AudioSource doorCreakSound;
+
+  
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            UIInteractionCanvas.gameObject.SetActive(true);
+
+            if (Input.GetKeyDown("e"))
+            {
+                openDoorAnimation.Play();
+                doorCreakSound.Play();
+                gameObject.GetComponent<BoxCollider>().enabled = false;
+                UIInteractionCanvas.gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        UIInteractionCanvas.gameObject.SetActive(false);
+    }
+
+
+
+
+
+
+
+
+}
