c59c82368f3debb4098a0584a4439bc6b8cc9872
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-06-11 18:52:13 +0800

    Pick up card and open door

---
+++ HorrorGame/Assets/GameStuff/Scriptes/Door/DoorScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private Animation anim;
+    public KeyCardManager keyCardManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            if (Input.GetKeyDown("e") && keyCardManager.YellowKeyCard == true)
+            {
+                anim.Play("DoorOpening");
+            }
+        }
+    }
+}
