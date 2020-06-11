bf2cb79a5336cf9e1321ad19a277e995f0145c1e
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-06-11 18:56:14 +0800

    Done with Door

--- HorrorGame/Assets/GameStuff/Scriptes/Door/DoorScript.cs
+++ HorrorGame/Assets/GameStuff/Scriptes/Door/DoorScript.cs
@@ -6,10 +6,12 @@ public class DoorScript : MonoBehaviour
 {
     private Animation anim;
     public KeyCardManager keyCardManager;
+    public GameObject textOpenup;
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animation>();
+        textOpenup.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,11 +25,17 @@ public class DoorScript : MonoBehaviour
 
         if (other.gameObject.CompareTag("Player"))
         {
+            textOpenup.SetActive(true);
 
             if (Input.GetKeyDown("e") && keyCardManager.YellowKeyCard == true)
             {
                 anim.Play("DoorOpening");
             }
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        textOpenup.SetActive(false);
     }
 }
