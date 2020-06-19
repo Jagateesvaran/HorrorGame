110fc548928db5ffb0f6df1858f211f252584362
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-06-19 16:21:53 +0800

    Throw object and show area of place

--- HorrorGame/Assets/GameStuff/Scriptes/PickUp.cs
+++ HorrorGame/Assets/GameStuff/Scriptes/PickUp.cs
@@ -1,61 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    public Transform theDest;
 
-    float throwForce = 600;
-    Vector3 objectPos;
-    float distance;
-
-    public bool canHold = true;
-    public GameObject item;
-    public GameObject tempParent;
-    public bool isHolding = false;
-
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-
-        distance = Vector3.Distance(item.transform.position, tempParent.transform.position);
-        if (distance >= 1f)
-        {
-            isHolding = false;
-        }
-        //Check if isholding
-        if (isHolding == true)
-        {
-            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            item.transform.SetParent(tempParent.transform);
-
-            if (Input.GetMouseButtonDown(1))
-            {
-                item.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
-                isHolding = false;
-            }
-        }
-        else
-        {
-            objectPos = item.transform.position;
-            item.transform.SetParent(null);
-            item.GetComponent<Rigidbody>().useGravity = true;
-            item.transform.position = objectPos;
-        }
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = theDest.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
     }
 
-    void OnMouseDown()
+    private void OnMouseUp()
     {
-        if (distance <= 1f)
-        {
-            isHolding = true;
-            item.GetComponent<Rigidbody>().useGravity = false;
-            item.GetComponent<Rigidbody>().detectCollisions = true;
-        }
+        this.GetComponent<Rigidbody>().AddForce(theDest.transform.forward * 600f);
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<BoxCollider>().enabled = true;
+
     }
-    void OnMouseUp()
-    {
-        isHolding = false;
-    }
+
 }
