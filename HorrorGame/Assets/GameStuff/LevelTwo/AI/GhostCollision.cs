e0f094c49f1b2e317c8b3f919ed8a1db413408e0
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-06-28 18:55:43 +0800

    Checkpoint

--- HorrorGame/Assets/GameStuff/LevelTwo/AI/GhostCollision.cs
+++ HorrorGame/Assets/GameStuff/LevelTwo/AI/GhostCollision.cs
@@ -1,37 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GhostCollision : MonoBehaviour
 {
-
-
-
+    public GameObject DiePanel;
     public Animator animator;
+    public bool istrigger;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        istrigger = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (istrigger == true)
+        {
+            StartCoroutine(coroutineA());
+           
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && istrigger == false)
         {
             Debug.Log("Ghost Hit Player");
             this.gameObject.GetComponentInChildren<Camera>().enabled = true;
+            this.gameObject.GetComponent<NavMeshAgent>().speed = 0;
             other.gameObject.GetComponentInChildren<Camera>().enabled = false;
             other.gameObject.GetComponentInChildren<CharacterController>().enabled = false;
             animator.SetBool("isWalking", false);
-
+            istrigger = true;
         }
+    }
+    IEnumerator coroutineA()
+    {
+        // wait for 1 second
+        yield return new WaitForSeconds(4.0f);
+        DiePanel.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
 }
---
+++ HorrorGame/Assets/GameStuff/LevelTwo/Script/Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public CollectibleManager collectibleManager;
+    private Animation anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown("e") && collectibleManager.currentAmount == 6)
+            {
+                this.gameObject.GetComponent<BoxCollider>().enabled = false;
+                anim.Play("HouseDoor");
+                //Debug.Log("Can Open Door");
+            }
+
+        }
+      
+    }
+}
