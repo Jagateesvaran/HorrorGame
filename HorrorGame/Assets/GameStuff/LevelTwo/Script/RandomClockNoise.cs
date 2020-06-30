bc4e66444fdef4192aaaf648ea504cdaa7242089
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-06-30 12:46:54 +0800

    Added Clock Sound Effect

---
+++ HorrorGame/Assets/GameStuff/LevelTwo/Script/RandomClockNoise.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomClockNoise : MonoBehaviour
+{
+
+    public AudioSource dongAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(PlayClockAudio());
+    }
+
+    private IEnumerator PlayClockAudio()
+    {
+        int x = Random.Range(300, 1200);
+        yield return new WaitForSeconds(x);
+        dongAudio.Play();
+
+    }
+
+
+}
