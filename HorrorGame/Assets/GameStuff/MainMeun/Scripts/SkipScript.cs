2fc8ec280a9f26a2a76ca72a58240f32c754fffb
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-07-01 19:32:39 +0800

    Added Scan Color and Skip Button

---
+++ HorrorGame/Assets/GameStuff/MainMeun/Scripts/SkipScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Detect when the Return key is pressed down
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+    }
+}
