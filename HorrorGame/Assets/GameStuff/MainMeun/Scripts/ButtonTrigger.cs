82f63c31bb14b5e88e7e0ddd1b19c39749615a66
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2020-07-04 20:36:11 +0800

    doing meain meun page

---
+++ HorrorGame/Assets/GameStuff/MainMeun/Scripts/ButtonTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    public Animator animator;
+    public LevelLoader levelLoader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.SetBool("Hover", false);
+        animator.SetBool("Pressed", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MouseHover()
+    {
+        animator.SetBool("Hover", true);
+    }
+
+    public void MouseExit()
+    {
+        animator.SetBool("Hover", false);
+    }
+
+    public void MousePressed()
+    {
+        animator.SetBool("Pressed", true);
+        StartCoroutine(Coroutine());
+    }
+
+    IEnumerator Coroutine()
+    {
+        yield return new WaitForSeconds(1);
+        levelLoader.LoadLevel("CutScene");
+    }
+}
